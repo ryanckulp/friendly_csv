35d7d1e0948c93daa52b1cce0466882252b7a129
Ryan Kulp <ryanckulp@gmail.com>
2019-10-11 10:57:36 +0800

    better uuid creation

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,11 +2,11 @@ class Batch < ActiveRecord::Base
   has_many :leads, dependent: :destroy
 
   before_create :generate_uuid
+
   def generate_uuid
-    until self.uuid.present?
-      random_id = SecureRandom.hex(16)
-      batch = Batch.find_by(uuid: random_id) # guarantee no overlap
-      self.uuid = random_id if batch.nil?
+    self.uuid = loop do
+      random_uuid = SecureRandom.hex(16)
+      break random_uuid unless self.class.exists?(uuid: random_uuid)
     end
   end
 end
